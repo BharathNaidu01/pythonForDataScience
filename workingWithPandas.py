117f2e9da5a22e7c484cc307cec7ab4d8fc2f29d
BharathNaidu01 <43294507+BharathNaidu01@users.noreply.github.com>
2021-01-24 11:19:47 +0530

    Add files via upload

---
+++ workingWithPandas.py
@@ -0,0 +1,16 @@
+#getting started with pandas library
+#series
+
+import pandas as pd
+
+
+cricket_grounds =  pd.Series(["Wankhade","Chinnaswamy","Chepauk"
+                              ,"Ranchi"])
+
+#here we are converting a list into a series. 
+#Here series is nothing but one dimensional
+
+
+#print(cricket_grounds)
+
+cricket_grounds[1]
