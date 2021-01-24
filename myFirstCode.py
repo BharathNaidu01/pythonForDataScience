ec1afef6bcb69d15ebf9860121fc772d8ec26b9b
BharathNaidu01 <43294507+BharathNaidu01@users.noreply.github.com>
2021-01-24 10:51:21 +0530

    Add files via upload

---
+++ myFirstCode.py
@@ -0,0 +1,9 @@
+name = "Bharath Naidu"
+print(name)
+#you are defining a variable called name and assigning values to it.
+
+name
+#you need not use print function to print any variable. But it would be a good practise
+#to use print
+
+# this(#) symbol is used for commenting
